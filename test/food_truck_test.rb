e8f0f0506978c26e1ac81a4a6c1774af749fa5d0
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-15 13:18:19 -0600

    Update test to check stock and inventory

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -16,9 +16,14 @@ class FoodTruckTest < Minitest::Test
     assert_equal ({}), food_truck.inventory
   end
 
-  def test_it_can_check_stock
+
+  def test_it_can_add_and_check_stock
     food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     assert_equal 0, food_truck.check_stock(item1)
+    food_truck.stock(item1, 30)
     assert_equal 30, food_truck.check_stock(item1, 30)
+    expected = {item1 => 30}
+    assert_equal expected, food_truck.inventory
   end
 end
