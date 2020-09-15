bbf0c572a8994620bf79a57ea7ad07c4a16f46fb
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-15 13:41:35 -0600

    Add test for attributes

--- test/event_test.rb
+++ test/event_test.rb
@@ -10,4 +10,10 @@ class EventTest < Minitest::Test
     event = Event.new("South Pearl Street Farmers Market")
     assert_instance_of Event, event
   end
+
+  def test_it_has_attributes
+    event = Event.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", event.name
+    assert_equal [], event.food_trucks
+  end
 end
