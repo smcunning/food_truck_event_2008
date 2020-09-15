a54a5cfe6d498c63a6d6e076a56d0bea403c4dce
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-15 13:12:18 -0600

    Add test for attributes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -9,4 +9,9 @@ class FoodTruckTest < Minitest::Test
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_instance_of FoodTruck, food_truck
   end
+
+  def test_it_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal "Rocky Mountain Pies", food_truck.name
+  end
 end
