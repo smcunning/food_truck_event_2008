9a74cf441aa83012a1dd71e1c43b3ba987a3d68d
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-15 13:22:31 -0600

    Add method to check stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,4 +4,12 @@ attr_reader :name, :inventory
     @name = name
     @inventory = {}
   end
+
+  def check_stock(item_obj)
+    if @inventory.include?(item_obj)
+      return item_obj.values
+    else
+      return 0
+    end
+  end
 end
