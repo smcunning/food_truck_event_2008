4fd806e27d85599accab0eed0c1ae5fcec63d814
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-15 13:15:35 -0600

    Add test to check stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -15,4 +15,10 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_equal ({}), food_truck.inventory
   end
+
+  def test_it_can_check_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal 0, food_truck.check_stock(item1)
+    assert_equal 30, food_truck.check_stock(item1, 30)
+  end
 end
