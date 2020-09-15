382353f85a403a15c4e18ce7fddab8ea7598f424
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-15 13:55:02 -0600

    Add method for food trucks that sell

--- lib/event.rb
+++ lib/event.rb
@@ -12,6 +12,16 @@ attr_reader :name, :food_trucks
   def food_truck_names
     @food_trucks.map do |food_truck|
       food_truck.name
-    end 
+    end
+  end
+
+  def food_trucks_that_sell(item_obj)
+    food_trucks = []
+    @food_trucks.each do |food_truck|
+      if food_truck.inventory.include?(item_obj) && food_truck.inventory[item_obj] > 0
+         food_trucks << food_truck
+       end
+     end
+     food_trucks
   end
 end
